db88ca908d0315f4aa58251fd146593558efcd65
Kaljo <nkaljo@gmail.com>
2015-06-26 18:26:51 +0200

    warrior attack

---
+++ Assets/Warrior/sword.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sword : MonoBehaviour {
+
+	public Collider playerBody;
+
+	void Start(){
+		Physics.IgnoreCollision (playerBody, GetComponent<Collider> ());
+	}
+
+	void OnCollisionEnter(Collision col){
+		col.gameObject.SendMessage ("hit", SendMessageOptions.DontRequireReceiver);
+	}
+}
