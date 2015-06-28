2fddf3d5c7e20c36db7564cb957d2f5f4ffe6b84
Kaljo <nkaljo@gmail.com>
2015-06-28 14:02:42 +0200

    warrior attack anim

--- Assets/Warrior/Warrior.cs
+++ Assets/Warrior/Warrior.cs
@@ -4,10 +4,13 @@ using System.Collections;
 public class Warrior : MonoBehaviour {
 
 	public GameObject player;
+	public Animator anim;
+
 
 	public float distance = 5f;
 	public KeyCode teleportLeft = KeyCode.Q;
 	public KeyCode teleportRight = KeyCode.E;
+	public KeyCode teleportForward = KeyCode.F;
 	
 	// Use this for initialization
 	void Start () {
@@ -16,6 +19,10 @@ public class Warrior : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (Input.GetKeyDown (teleportForward)) {
+			player.transform.position += new Vector3(transform.forward.x, 0, transform.forward.z) * distance;
+		}
 
 		if (Input.GetKeyDown(teleportLeft)){
 			player.transform.position -= transform.right * distance;
@@ -29,5 +36,17 @@ public class Warrior : MonoBehaviour {
 			player.transform.position += new Vector3(0,1,0) * distance;
 		}
 
+		if (Input.GetMouseButtonDown(0)){
+			attack();
+		}
+
 	}
+
+	private void attack(){
+		anim.SetTrigger ("click");
+	}
+
 }
+
+
+
