c6881259e539cd4fed7ed5fefd0b7b2d9ea46708
Kaljo <nkaljo@gmail.com>
2015-06-30 01:14:46 +0200

    bad synching via script done

---
+++ Assets/NetworkUnit.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkUnit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
+
+		if (stream.isWriting) {
+			//send
+
+			stream.SendNext(transform.position);
+			stream.SendNext(transform.rotation);
+
+		} else {
+			//receive
+			transform.position = (Vector3)stream.ReceiveNext();
+			transform.rotation = (Quaternion)stream.ReceiveNext();
+		}
+
+	}
+}
